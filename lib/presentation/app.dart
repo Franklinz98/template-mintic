4bfe35a5c5b2eb1c0a9e7fa74a814401ba79af68
Franklinz98 <franklinz@uninorte.edu.co>
2021-07-26 20:18:10 -0500

    firebase initialization

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:red_egresados/domain/use_case/controller.dart';
@@ -17,6 +18,26 @@ class _AppState extends State<App> {
   // Dependency injection: setting up state management
   final Controller controller = Get.put(Controller());
 
+  // Firebase initialization vars
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
   @override
   void initState() {
     // State management: listening for changes on currentUser
@@ -29,11 +50,31 @@ class _AppState extends State<App> {
         Get.off(() => Authentication());
       }
     });
+    // Trigger Firebase initialization
+    initializeFlutterFire();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    // Show error message if initialization failed
+    if (_error) {
+      return MaterialApp(
+        home: Center(
+          child: Text("Hubo un error con Firebase"),
+        ),
+      );
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return MaterialApp(
+        home: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return GetMaterialApp(
       theme: MyTheme.ligthTheme,
       darkTheme: MyTheme.darkTheme,
